ecf32bb79327f46389d64d7061b2e34f5acff061
Paul Miller <paul@paulmillr.com>
2014-05-26 23:48:00 -0400

    Tight! Iterating over each question and over each question`s answers without a hitch. This is going to be my stopping point for tonight. Next up I`m going to get this json pulled from local storage. After that I`ll let you submit the json to local storage through the config screen.

--- js/flashcard.js
+++ js/flashcard.js
@@ -2,11 +2,29 @@ var flashcard = angular.module('flashcard', []);
 
 flashcard.controller('FlashcardCtrl', function ($scope) {
   $scope.questions = [
-    {'question': 'How\'s this working?',
-     'answer': 'Pretty good.'},
-    {'question': 'How\'s this working now?',
-     'answer': 'Iteration is working.'},
-    {'question': 'How\'s this working?',
-     'answer': 'Three answers! Nice.'}
+		{'question': 'Are lamps bright?',
+			'answers': [
+				'Dude. Yeah.',
+				'...No, not really...?',
+				'Depends on the lamp.',
+				'My lamp is bright as hell, tbvh.'
+			]
+		},
+		{'question': 'Do you like radiators?',
+			'answers': [
+				'Gotta have paint on em\'.',
+				'...No, not really...?',
+				'Depends on the radiator.',
+				'My radiator is swag as hell, tbvh.'
+			]
+		},
+		{'question': 'What did you eat for dinner?',
+			'answers': [
+				'A burrito.',
+				'A burrito with chips',
+				'Doritos Locos taco doritos ice cream.',
+				'Big bag of expired lollipops.'
+			]
+		}
   ];
 });
