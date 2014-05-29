f793259efa147120620f88079d2e64062ef09640
Alex Vance <alex@blueantlabs.com>
2014-05-28 22:04:52 -0400

    nav buttons hooked up to switch btwn quiz/config

--- js/flashcard-quiz.js
+++ js/flashcard-quiz.js
@@ -1,7 +1,17 @@
 (function($){
 	$(function(){
 		//quiz progression
-		console.log('flashcardquiz');
+		$("#quiz-config").on("click", function(e){
+			e.preventDefault();
+			$(".section-config").removeClass("hide");
+			$(".section-quiz").addClass("hide");
+		});
+		$("#quiz-take").on("click", function(e){
+			e.preventDefault();
+			$(".section-quiz").removeClass("hide");
+			$(".section-config").addClass("hide");
+			$(".quiz-cards .quiz-card").first().removeClass("hide");
+		});
 		
 	});
 })(jQuery);
