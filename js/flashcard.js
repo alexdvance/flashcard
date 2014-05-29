ff973db195553d28b263e615222bab7524f7206e
Alex Vance <alex@blueantlabs.com>
2014-05-28 21:30:21 -0400

    got storage.set and storage.get working. storage.get wasnt reading the json i put in local storage properly until i used storage.set to put it in there. guess storage.set formatted it correctly. next im gonna take a break from all this and focus on getting the flow of the quiz right so one can actually take it.

--- js/flashcard.js
+++ js/flashcard.js
@@ -1,32 +1,34 @@
 var flashcard = angular.module('flashcard', ['angularLocalStorage', 'ngCookies']);
 
-flashcard.controller('FlashcardCtrl', function($scope) {
+flashcard.controller('FlashcardCtrl', function($scope, storage) {
+	storage.set('test','test value');
   // storage.bind($scope,'varName',{defaultValue: 'randomValue123' ,storeName: 'customStoreKey'});
-  $scope.questions = [
-		{'question': 'Are lamps bright?',
-			'answers': [
-				'Dude. Yeah.',
-				'...No, not really...?',
-				'Depends on the lamp.',
-				'My lamp is bright as hell, tbvh.'
+  var questionJson = [
+		{"question": "Are lamps bright?",
+			"answers": [
+				"Dude. Yeah.",
+				"...No, not really...?",
+				"Depends on the lamp.",
+				"My lamp is bright as hell, tbvh."
 			]
 		},
-		{'question': 'Do you like radiators?',
-			'answers': [
-				'Gotta have paint on em\'.',
-				'...No, not really...?',
-				'Depends on the radiator.',
-				'My radiator is swag as hell, tbvh.'
+		{"question": "Do you like radiators?",
+			"answers": [
+				"Gotta have paint on em\".",
+				"...No, not really...?",
+				"Depends on the radiator.",
+				"My radiator is swag as hell, tbvh."
 			]
 		},
-		{'question': 'What did you eat for dinner?',
-			'answers': [
-				'A burrito.',
-				'A burrito with chips',
-				'Doritos Locos taco doritos ice cream.',
-				'Big bag of expired lollipops.'
+		{"question": "What did you eat for dinner?",
+			"answers": [
+				"A burrito.",
+				"A burrito with chips",
+				"Doritos Locos taco doritos ice cream.",
+				"Big bag of expired lollipops."
 			]
 		}
   ];
-  // $scope.questions = storage.get('flashcard-app-questions');
+  storage.set('test2', questionJson);
+  $scope.questions = storage.get('test2');
 });
