98b2c798e7a50b2fe6c2aecb29bb2838f9dd2050
Alex Vance <alex@blueantlabs.com>
2014-10-06 01:33:21 -0400

    took out stock quiz

--- js/flashcard.js
+++ js/flashcard.js
@@ -1,50 +1,71 @@
-var flashcard = angular.module('flashcard', ['mongolabResource', 'angularLocalStorage', 'ngCookies']);
+var flashcard = angular.module('flashcard', []);
 
-flashcard.constant('API_KEY', '2oG0UQM7BSXLG2XtawERrJtviFj5yzTf');
-flashcard.constant('DB_NAME', 'flashcard');
-
-flashcard.factory('Project', function ($mongolabResource) {
-    return $mongolabResource('projects');
+flashcard.controller('FlashcardQuizCtrl', function($scope, $http) {
+	$scope.takeQuiz = function() {
+		$http.get('https://api.mongolab.com/api/1/databases/flashcard/collections/QUESTIONS/?apiKey=2oG0UQM7BSXLG2XtawERrJtviFj5yzTf').
+			success(function(data, status, headers, config) {
+				// this callback will be called asynchronously
+				// when the response is available
+				$scope.questions = data;
+				$scope.$apply();
+			}).
+			error(function(data, status, headers, config) {
+				// called asynchronously if an error occurs
+				// or server returns response with an error status.
+				console.log('error');
+			});
+	};
 });
 
-flashcard.controller('FlashcardCtrl', function($scope, storage, Project) {
-	// storage.bind($scope,'question-1',{defaultValue: 'Are lamps bright?' ,storeName: 'question-1'});
-	// storage.bind($scope,'varName',{defaultValue: 'randomValue123' ,storeName: 'customStoreKey'});
-	// var restUrl = "https://api.mongolab.com/api/1/databases?apiKey="
-  $scope.projects = Project.query();
+flashcard.controller('FlashcardConfigCtrl', function($scope, $http) {
+	$scope.submitQuestionConfig = function() {
+		$scope.id = 1;
+		$http.put('https://api.mongolab.com/api/1/databases/flashcard/collections/QUESTIONS/' + 
+			 $scope.id + '?apiKey=2oG0UQM7BSXLG2XtawERrJtviFj5yzTf',
+		{'question'  : $scope.question1,
+				'answers': [
+					$scope.answer1_1,
+					$scope.answer1_2,
+					$scope.answer1_3,
+					$scope.answer1_4
+				 ],
+				 'correct' : $scope.correct
+		}).success(function (data, status, headers, config) {
+					 // $scope.refreshGrid();
+					 // $scope.id = '';
+					 // $scope.party = {}
+					 // $scope.SaveButton = "Add";
+		}).error(function (data, status, headers, config) { alert(status) });
+	};
 
-	var defaultQuestionJson = [
-		{"question": "Are lamps bright?",
-			"answers": [
-				"Dude. Yeah.",
-				"...No, not really...?",
-				"Depends on the lamp.",
-				"My lamp is bright as hell, tbvh."
-			],
-			"correct" : 3
-		},
-		{"question": "Do you like radiators?",
-			"answers": [
-				"Gotta have paint on em\".",
-				"...No, not really...?",
-				"Depends on the radiator.",
-				"My radiator is swag as hell, tbvh."
-			],
-			"correct" : 4
-		},
-		{"question": "What did you eat for dinner?",
-			"answers": [
-				"A burrito.",
-				"A burrito with chips",
-				"Doritos Locos taco doritos ice cream.",
-				"Big bag of expired lollipops."
-			],
-			"correct" : 2
-		}
-	];
-	storage.set('questionKey', defaultQuestionJson);
-	$scope.questions = storage.get('questionKey');
-
-	$scope.question1 = $scope.questions[0].question;
+// 	var defaultQuestionJson = [
+// 		{"question": "Are lamps bright?",
+// 			"answers": [
+// 				"Dude. Yeah.",
+// 				"...No, not really...?",
+// 				"Depends on the lamp.",
+// 				"My lamp is bright as hell, tbvh."
+// 			],
+// 			"correct" : 3
+// 		},
+// 		{"question": "Do you like radiators?",
+// 			"answers": [
+// 				"Gotta have paint on em\".",
+// 				"...No, not really...?",
+// 				"Depends on the radiator.",
+// 				"My radiator is swag as hell, tbvh."
+// 			],
+// 			"correct" : 4
+// 		},
+// 		{"question": "What did you eat for dinner?",
+// 			"answers": [
+// 				"A burrito.",
+// 				"A burrito with chips",
+// 				"Doritos Locos taco doritos ice cream.",
+// 				"Big bag of expired lollipops."
+// 			],
+// 			"correct" : 2
+// 		}
+// 	];
 
 });
